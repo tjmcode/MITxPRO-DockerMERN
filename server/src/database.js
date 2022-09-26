c47b0adb858b720485a21deb24ce927bfcf0a04d
Timothy J McGuire <tmcguire@mcode.com>
2022-09-26 16:12:56 -0400

    Move build to 'appname' everywhere

--- server/src/database.js
+++ server/src/database.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const User = require('./models/user.model');
 
 // for running native on local machine
-// const connection = 'mongodb://localhost:27017/dockerize-me';
+// const connection = 'mongodb://localhost:27017/appname';
 
 // for running in a Docker Container that's running a HOST named "mongo"
 // NOTE: This "mongo" is *not* the Docker Container Name!
-//                  mondodb://host:port/app
-const connection = 'mongodb://appname-database:27017/dockerize-me';
+//                  mondodb://host:port/appname
+const connection = 'mongodb://appname-database:27017/appname';
 
 const connectDb = () =>
 {
